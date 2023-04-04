beb2f069060ae658315e5dffa3d20e9778b8f1ae
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2023-04-04 16:03:49 +0200

    Adding thing

--- SQL/importation.sql
+++ SQL/importation.sql
@@ -1,7 +1,12 @@
 DROP TABLE IF EXISTS import CASCADE;
 DROP TABLE IF EXISTS noc;
 DROP TABLE IF EXISTS athlete CASCADE;
-DROP TABLE IF EXISTS measurement CASCADE;
+DROP TABLE IF EXISTS regions CASCADE;
+DROP TABLE IF EXISTS olympics CASCADE;
+DROP TABLE IF EXISTS epreuves CASCADE;
+DROP TABLE IF EXISTS participe;
+DROP TABLE IF EXISTS resultat;
+DROP TABLE IF EXISTS contient;
 
 CREATE temp TABLE import (
     n1 INT, n2 TEXT, n3 TEXT, n4 INT, n5 INT, n6 FLOAT,
@@ -10,17 +15,6 @@ CREATE temp TABLE import (
     );
     
 \copy import from 'Ressource/athlete_events_utf8.csv' with (FORMAT csv, NULL 'NA', HEADER, ENCODING 'UTF-8')
-
-ALTER TABLE import ALTER n2 TYPE CHAR(108);
-ALTER TABLE import ALTER n3 TYPE CHAR(1);
-ALTER TABLE import ALTER n7 TYPE CHAR(47);
-ALTER TABLE import ALTER n8 TYPE CHAR(3);
-ALTER TABLE import ALTER n9 TYPE CHAR(11);
-ALTER TABLE import ALTER n11 TYPE CHAR(6);
-ALTER TABLE import ALTER n12 TYPE CHAR(22);
-ALTER TABLE import ALTER n13 TYPE CHAR(25);
-ALTER TABLE import ALTER n14 TYPE CHAR(85);
-ALTER TABLE import ALTER n15 TYPE CHAR(6);
 
 DELETE FROM import WHERE n10 < 1920 OR n13 = 'Art Competitions';
 
@@ -32,28 +26,93 @@ CREATE temp TABLE noc (
 
 \copy noc from 'Ressource/noc_regions_utf8.csv' with (FORMAT csv, HEADER, ENCODING 'UTF-8')
 
-/*ALTER TABLE import ALTER n16 TYPE CHAR(3);
-ALTER TABLE import ALTER n17 TYPE CHAR(32);
-ALTER TABLE import ALTER n18 TYPE CHAR(27);*/
+-- 
+
+CREATE TABLE athlete (
+    id INT,
+    nom CHAR(108) NOT NULL,
+    genre CHAR(1),
+    taille INT,
+    poids FLOAT,
+    CONSTRAINT pk_athele PRIMARY KEY (id));
+
+INSERT INTO athlete
+    SELECT DISTINCT n1, n2, n3, n5, n6
+    FROM import ORDER BY n1;
 
 
-SELECT DISTINCT n1 AS id, n2 AS name, n3 AS gender, n4 AS age, n7 AS team, n8 AS noc
-INTO athlete FROM import;
+CREATE TABLE regions (
+    noc CHAR(3),
+    regions CHAR(32),
+    note CHAR(27),
+    CONSTRAINT pk_region PRIMARY KEY (noc));
 
-ALTER TABLE athlete ADD CONSTRAINT pk_athele PRIMARY KEY (id);
+INSERT INTO regions
+    SELECT DISTINCT n1, n2, n3
+    FROM noc ORDER BY n1;
 
-SELECT DISTINCT n1 AS id, n5 AS height, n6 AS weight, n10 AS year
-INTO measurement FROM import;
+UPDATE regions SET noc = 'SGP' WHERE noc = 'SIN';
 
-ALTER TABLE measurement ADD CONSTRAINT fk_athete FOREIGN KEY (id) REFERENCES athlete(id);
 
-SELECT DISTINCT n9 AS games, n10 AS year, n11 AS season, n12 AS city
-INTO olympics FROM import;
+CREATE TABLE olympics (
+    annee INT,
+    saison CHAR(6),
+    ville CHAR(22),
+    label CHAR(11),
+    CONSTRAINT pk_olympics PRIMARY KEY (annee, saison, ville));
 
-ALTER TABLE olympics ADD CONSTRAINT pk_olympics PRIMARY KEY (year, season, city);
+INSERT INTO olympics
+    SELECT DISTINCT n10, n11, n12, n9
+    FROM import ORDER BY n10;
 
-SELECT DISTINCT n14 AS wording, n13 AS sport, n15 AS medal, n1 AS id
-INTO events FROM import;
 
-ALTER TABLE events ADD CONSTRAINT pk_events PRIMARY KEY (wording);
-ALTER TABLE events ADD CONSTRAINT fk_olympics FOREIGN KEY (id) REFERENCES athlete(id);
+CREATE TABLE epreuves (
+    label CHAR(85),
+    sport CHAR(25),
+    CONSTRAINT pk_events PRIMARY KEY (label));
+
+INSERT INTO epreuves
+    SELECT DISTINCT n14, n13
+    FROM import ORDER BY n13;
+
+
+CREATE TABLE participe (
+    id INT,
+    saison CHAR(8),
+    annee INT,
+    ville CHAR(22),
+    noc CHAR(3),
+    equipe CHAR(47),
+    age INT,
+    CONSTRAINT fk_athlete FOREIGN KEY (id) REFERENCES athlete(id),
+    CONSTRAINT fk_olympics FOREIGN KEY (saison, annee, ville) REFERENCES olympics(saison, annee, ville),
+    CONSTRAINT fk_regions FOREIGN KEY (noc) REFERENCES regions(noc));
+
+INSERT INTO participe
+    SELECT DISTINCT n1, n11, n10, n12, n8, n7, n4 
+    FROM import ORDER BY n1;
+
+
+CREATE TABLE resultat (
+    id INT,
+    label CHAR(85),
+    medaille CHAR(6),
+    CONSTRAINT fk_athlete FOREIGN KEY (id) REFERENCES athlete(id),
+    CONSTRAINT fk_epreuves FOREIGN KEY (label) REFERENCES epreuves(label));
+
+INSERT INTO resultat
+    SELECT DISTINCT n1, n14, n15
+    FROM import ORDER BY n1;
+
+
+CREATE TABLE contient (
+    saison CHAR(6),
+    annee INT,
+    ville CHAR(22),
+    label CHAR(85),
+    CONSTRAINT fk_olympics FOREIGN KEY (saison, annee, ville) REFERENCES olympics(saison, annee, ville),
+    CONSTRAINT fk_epreuves FOREIGN KEY (label) REFERENCES epreuves(label));
+
+INSERT INTO contient
+    SELECT DISTINCT n11, n10, n12, n
+    FROM import ORDER BY n1;
