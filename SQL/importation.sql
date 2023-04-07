fc39c77d5fd3868226d9eed6d3851168f5a57efe
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2023-04-07 10:37:31 +0200

    Adding log in importation.sql

--- SQL/importation.sql
+++ SQL/importation.sql
@@ -7,27 +7,41 @@ DROP TABLE IF EXISTS epreuves CASCADE;
 DROP TABLE IF EXISTS participe;
 DROP TABLE IF EXISTS resultat;
 DROP TABLE IF EXISTS contient;
+\! echo ""
 
+
+\! echo "Création de la table temporaire import"
 CREATE temp TABLE import (
-    n1 INT, n2 TEXT, n3 TEXT, n4 INT, n5 INT, n6 FLOAT,
-    n7 TEXT, n8 TEXT, n9 TEXT, n10 INT, n11 TEXT, n12 TEXT,
-    n13 TEXT, n14 TEXT, n15 TEXT
-    );
-    
+    id INT, nom TEXT, genre TEXT, age INT, taille INT, poids FLOAT,
+    equipe TEXT, noc TEXT, label TEXT, annee INT, saison TEXT, ville TEXT,
+    sport TEXT, elabel TEXT, medaille TEXT);
+\! echo ""
+
+
+\! echo "Import des données d'athlete_events_utf8.csv"
 \copy import from 'Ressource/athlete_events_utf8.csv' with (FORMAT csv, NULL 'NA', HEADER, ENCODING 'UTF-8')
+\! echo ""
 
-DELETE FROM import WHERE n10 < 1920 OR n13 = 'Art Competitions';
 
--- SELECT COUNT(*) FROM import; -> 255.080
+\! echo "Suppression des données inutiles"
+DELETE FROM import WHERE annee < 1920 OR sport = 'Art Competitions';
+\! echo ""
 
+
+\! echo "Création de la table temporaire noc"
 CREATE temp TABLE noc (
-    n1 TEXT, n2 TEXT, n3 TEXT
+    noc TEXT, regions TEXT, notes TEXT
     );
+\! echo ""
 
+
+\! echo "Import des données d'noc_regions_utf8.csv"
 \copy noc from 'Ressource/noc_regions_utf8.csv' with (FORMAT csv, NULL '', HEADER, ENCODING 'UTF-8')
+\! echo ""
 
 -- 
 
+\! echo "Création de la table athlete"
 CREATE TABLE athlete (
     id INT,
     nom CHAR(108) NOT NULL,
@@ -35,47 +49,61 @@ CREATE TABLE athlete (
     taille INT,
     poids FLOAT,
     CONSTRAINT pk_athele PRIMARY KEY (id));
+\! echo ""
 
+\! echo "Ajout des données dans athlete"
 INSERT INTO athlete
-    SELECT DISTINCT n1, n2, n3, n5, n6
-    FROM import ORDER BY n1;
+    SELECT DISTINCT id, nom, genre, taille, poids
+    FROM import ORDER BY id;
+\! echo ""
 
-
+\! echo "Création de la table regions"
 CREATE TABLE regions (
     noc CHAR(3),
     regions CHAR(32),
     note CHAR(27),
     CONSTRAINT pk_region PRIMARY KEY (noc));
+\! echo ""
 
+\! echo "Ajout des données dans regions"
 INSERT INTO regions
-    SELECT DISTINCT n1, n2, n3
-    FROM noc ORDER BY n1;
+    SELECT DISTINCT noc, regions, notes
+    FROM noc ORDER BY noc;
+\! echo ""
 
+\! echo "Transformation du noc 'SIN' en 'SGP' pour coller au données d'import"
 UPDATE regions SET noc = 'SGP' WHERE noc = 'SIN';
+\! echo ""
 
-
+\! echo "Création de la table olympics"
 CREATE TABLE olympics (
     annee INT,
     saison CHAR(6),
     ville CHAR(22),
     label CHAR(11),
     CONSTRAINT pk_olympics PRIMARY KEY (annee, saison, ville));
+\! echo ""
 
+\! echo "Ajout des données dans regions"
 INSERT INTO olympics 
-    SELECT DISTINCT n10, n11, n12, n9
-    FROM import ORDER BY n10;
+    SELECT DISTINCT annee, saison, ville, label
+    FROM import ORDER BY annee;
+\! echo ""
 
-
+\! echo "Création de la table epreuves"
 CREATE TABLE epreuves (
     label CHAR(85),
     sport CHAR(25),
     CONSTRAINT pk_events PRIMARY KEY (label));
+\! echo ""
 
+\! echo "Ajout des données dans epreuves"
 INSERT INTO epreuves
-    SELECT DISTINCT n14, n13
-    FROM import ORDER BY n13;
+    SELECT DISTINCT elabel, sport
+    FROM import ORDER BY sport;
+\! echo ""
 
-
+\! echo "Création de la table participe"
 CREATE TABLE participe (
     id INT,
     annee INT,
@@ -86,12 +114,15 @@ CREATE TABLE participe (
     CONSTRAINT fk_athlete FOREIGN KEY (id) REFERENCES athlete(id),
     CONSTRAINT fk_olympics FOREIGN KEY (annee, saison, ville) REFERENCES olympics(annee, saison, ville),
     CONSTRAINT fk_regions FOREIGN KEY (noc) REFERENCES regions(noc));
+\! echo ""
 
+\! echo "Ajout des données dans participe"
 INSERT INTO participe
-    SELECT DISTINCT n1, n10, n11, n12, n8, n4 
-    FROM import ORDER BY n1;
+    SELECT DISTINCT id, annee, saison, ville, noc, age 
+    FROM import ORDER BY id;
+\! echo ""
 
-
+\! echo "Création de la table resultat"
 CREATE TABLE resultat (
     id INT,
     label CHAR(85),
@@ -103,7 +134,10 @@ CREATE TABLE resultat (
     CONSTRAINT fk_athlete FOREIGN KEY (id) REFERENCES athlete(id),
     CONSTRAINT fk_epreuves FOREIGN KEY (label) REFERENCES epreuves(label),
     CONSTRAINT fk_olympics FOREIGN KEY (annee, saison, ville) REFERENCES olympics(annee, saison, ville));
+\! echo ""
 
+\! echo "Ajout des données dans resultat"
 INSERT INTO resultat
-    SELECT DISTINCT n1, n14, n10, n11, n12, n7, n15
-    FROM import ORDER BY n1;
+    SELECT DISTINCT id, elabel, annee, saison, ville, equipe, medaille
+    FROM import ORDER BY id;
+\! echo ""
