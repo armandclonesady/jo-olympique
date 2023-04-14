886c26f13578d3e6f4826b0d71ead1f8a5ff716c
Armand SADY <armand.sady.etu@univ-lille.fr>
2023-04-14 15:15:28 +0200

    FINISHED

--- SQL/requetes.sql
+++ SQL/requetes.sql
@@ -92,35 +92,37 @@ FROM olympics AS o JOIN resultat USING(annee, saison, ville) NATURAL JOIN epreuv
 GROUP BY o.label, o.ville;
 
 -- QUESTION 11
-SELECT o.annee, COUNT(*) AS nbMedaillesFemme
-FROM olympics AS o NATURAL JOIN resultat as r NATURAL JOIN athlete AS a
-WHERE a.genre = 'F' AND o.saison = 'Summer'
-GROUP BY o.annee;
-
+*/
 -- EXERCICE 6
 -- Les requêtes sont faites pour la Chine et le tennis de table:
 -- Requête 
 
-SELECT a.nom, COUNT(*) as nbMedaillesOr
-FROM resultat AS r, athlete AS a
-WHERE r.id = a.id 
-AND r.equipe = 'China' 
-AND r.medaille = 'Gold' 
+
+
+
+
+
+
+
+
+
+-- Requête 
+SELECT COUNT(DISTINCT r.id)
+FROM resultat AS r
+WHERE r.equipe = 'China'
+AND r.label LIKE 'Table Tennis%';
+
+-- Requête 
+SELECT ROUND(AVG(a.taille)::numeric, 2) AS tailleMoyenne, ROUND(AVG(a.poids)::numeric, 2) AS poidsMoyen, ROUND(AVG(p.age)::numeric, 2) AS ageMoyen
+FROM athlete AS a, resultat AS r, participe AS p
+WHERE a.id = r.id AND a.id = p.id
+AND r.equipe = 'China'
 AND r.label LIKE 'Table Tennis%'
-GROUP BY a.nom
-HAVING COUNT(*) <= MAX((SELECT DISTINCT COUNT(*) FROM resultat AS r2 WHERE r2.equipe = 'China' AND r2.medaille = 'Gold' AND r2.label LIKE 'Table Tennis%' GROUP BY r2.id));
-
-/*
-SELECT a.nom, COUNT(*) AS nbMedaillesOr
+AND a.genre = 'M';
+-- Requête 
+SELECT DISTINCT a.nom
 FROM resultat AS r, athlete AS a
-WHERE r.id = a.id 
-AND r.equipe = 'China' 
-AND r.medaille = 'Gold' 
+WHERE r.id = a.id
+AND r.equipe = 'China'
 AND r.label LIKE 'Table Tennis%'
-GROUP BY a.nom;
-*/
--- Requête 
-
--- Requête 
-
--- Requête 
+AND r.medaille IS NULL;
