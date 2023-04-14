9d54718c052158281d8a0b00f2d39b0b4e366da2
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2023-04-14 10:53:32 +0200

    ALmost finished

--- SQL/requetes.sql
+++ SQL/requetes.sql
@@ -38,11 +38,62 @@ SELECT rg.regions, COUNT(*) AS nbOr
 FROM participe NATURAL JOIN resultat AS rs NATURAL JOIN regions AS rg
 WHERE rs.medaille = 'Gold'
 GROUP BY rg.regions
-ORDER BY nbGold DESC;
+ORDER BY nbOr DESC;
 
 -- QUESTION 3
 SELECT rg.regions, COUNT(*) AS nbMedailles
 FROM participe NATURAL JOIN resultat AS rs NATURAL JOIN regions AS rg
 WHERE rs.medaille IS NOT NULL
 GROUP BY rg.regions
-ORDER BY nbGold DESC;
+ORDER BY nbMedailles DESC;
+
+-- QUESTION 4
+SELECT a.id, a.nom, COUNT(*) AS nbMedaillesOr
+FROM athlete AS a NATURAL JOIN resultat AS r
+WHERE r.medaille = 'Gold'
+GROUP BY a.id, a.nom
+ORDER BY nbMedaillesOr DESC;
+
+-- QUESTION 5
+SELECT rg.regions, COUNT(*) AS nbMedailles
+FROM participe NATURAL JOIN resultat AS rs NATURAL JOIN regions AS rg
+WHERE rs.ville = 'Albertville' AND rs.medaille IS NOT NULL
+GROUP BY rg.regions
+ORDER BY nbMedailles DESC;
+
+-- QUESTION 6
+/*SELECT COUNT(*)
+FROM participe AS p1 JOIN participe AS p2 NATURAL JOIN 
+WHERE p1.id == p2.ID
+AND p1.*/
+
+-- QUESTION 7
+
+-- QUESTION 8
+SELECT p.age, COUNT(*) AS nbOr
+FROM participe AS p NATURAL JOIN resultat AS rs
+WHERE rs.medaille = 'Gold' AND p.age IS NOT NULL
+GROUP BY p.age;
+
+-- QUESTION 9
+SELECT e.sport, COUNT(*) AS nbMedailles
+FROM epreuves AS e NATURAL JOIN resultat AS rs NATURAL JOIN participe AS p
+WHERE p.age >= 50 AND rs.medaille IS NOT NULL
+GROUP BY e.sport
+ORDER BY nbMedailles DESC;
+
+-- QUESTION 10
+SELECT o.label, o.ville, COUNT(DISTINCT e.elabel) AS nbEpreuves
+FROM olympics AS o JOIN resultat USING(annee, saison, ville) NATURAL JOIN epreuves AS e
+GROUP BY o.label, o.ville;
+
+-- QUESTION 11
+
+-- EXERCICE 6
+-- Requête 
+
+-- Requête 
+
+-- Requête 
+
+-- Requête 
